81b681c5f4e6648d01b78ae5344420d53e3f6829
marcoswmv <marcoswmv@gmail.com>
2020-08-02 01:29:15 +0300

    Added Data base manager class

---
+++ myContacts/Data/Managers/DataBaseManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataBaseManager.swift
+//  myContacts
+//
+//  Created by Marcos Vicente on 02.08.2020.
+//  Copyright © 2020 Antares Software Group. All rights reserved.
+//
+
+import Foundation
+import Contacts
+import RealmSwift
+
+class DataBaseManager {
+    
+}
