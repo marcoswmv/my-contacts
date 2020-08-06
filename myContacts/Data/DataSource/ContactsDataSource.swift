8ee62844a2129b1170e1d16d007f5ff24eba3110
marcoswmv <marcoswmv@gmail.com>
2020-08-06 18:18:42 +0300

    Added contacts data source + implemented methods for contacts and db managers

---
+++ myContacts/Data/DataSource/ContactsDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  ContactsDataSource.swift
+//  myContacts
+//
+//  Created by Marcos Vicente on 06.08.2020.
+//  Copyright © 2020 Antares Software Group. All rights reserved.
+//
+
+import UIKit
+
+class ContactsDataSource: BaseDataSource {
+
+}
